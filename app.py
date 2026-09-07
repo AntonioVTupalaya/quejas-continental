d00023f58c325b2a3ccee0cbf8b213260fa6e1ce
AntonioVTupalaya <antonio_vtupalaya@github.com>
2026-09-07 17:06:43 -0500

    Corrige contactos en pie de pagina

--- app.py
+++ app.py
@@ -445,7 +445,7 @@ def pie():
             <span class="uc-pie">·</span>
             <span class="uc-pie-daa">Dirección de Estudios Generales Ciencias</span>
             <span class="uc-pie">·</span>
-            <span class="uc-pie-at">@mquispe/@avelasquez</span>
+            <span class="uc-pie-at">@mquispee/@avelasquezt</span>
         </div>
         """,
         unsafe_allow_html=True,
